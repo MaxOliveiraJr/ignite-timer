a60a184fc66884fc5ae10c4b0ca785ea190ed93f
Max Oliveira <max-oliveira@outlook.com.br>
2025-04-29 18:01:38 -0300

    create header

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+    return (
+
+        <div>
+            <form action="">
+                <label htmlFor="">Vou tral</label>
+            </form>
+        </div>
+
+    )
+}
