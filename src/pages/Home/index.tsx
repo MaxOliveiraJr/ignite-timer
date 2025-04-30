5f2eb81c0c5668ddf3a6018d449a5f4b33161343
Max Oliveira <max-oliveira@outlook.com.br>
2025-04-29 21:23:36 -0300

    add home page e components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,40 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountButton, TaskInput } from "./styles";
+
 export function Home() {
     return (
 
-        <div>
+        <HomeContainer>
             <form action="">
-                <label htmlFor="">Vou tral</label>
+                <FormContainer>
+                    <label htmlFor="task">Vou tralalhar em</label>
+                    <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1"></option>
+                        <option value="Projeto 2"></option>
+                        <option value="Projeto 3"></option>
+                    </datalist>
+                    <label htmlFor="minutesAmount"> durante</label>
+                    <MinutesAmountInput id="minutesAmount" type="number" placeholder="00" step={5} max={60} min={5} />
+                    <span>minutos.</span>
+                </FormContainer>
+
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountButton type="submit">
+                    <Play size={24} />
+                    Começar
+                </StartCountButton>
             </form>
-        </div>
+        </HomeContainer>
 
     )
 }
